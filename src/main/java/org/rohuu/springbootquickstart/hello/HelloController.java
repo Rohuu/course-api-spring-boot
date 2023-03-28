84b24d00acfcca62a9ee71b1625d90ce47e21524
Rohuu <rohuu99022@gmail.com>
2023-03-28 16:45:48 +0530

    adding a REST controller

---
+++ src/main/java/org/rohuu/springbootquickstart/hello/HelloController.java
@@ -0,0 +1,15 @@
+package org.rohuu.springbootquickstart.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    // whenever a http req comes to this application at the url /hello, then run this method
+    // @RequestMapping maps only to THE GET method by default
+    public String sayHi() {
+        return "Hi!";
+    }
+
+}
