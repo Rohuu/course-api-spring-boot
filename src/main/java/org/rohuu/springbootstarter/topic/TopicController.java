0ff4b0f08e2b763d8f82dc8b62cf817149a59f30
Rohuu <rohuu99022@gmail.com>
2023-03-29 14:03:49 +0530

    getting a single resource which is getting single topic from allTopics

--- src/main/java/org/rohuu/springbootstarter/topic/TopicController.java
+++ src/main/java/org/rohuu/springbootstarter/topic/TopicController.java
@@ -1,6 +1,7 @@
 package org.rohuu.springbootstarter.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,10 @@ public class TopicController {
     @RequestMapping("/topics")
     public List getAllTopics(){
         return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}") // parenthesis is for variable part
+    public Topic getTopic(@PathVariable String id){ // @PathVariable tells spring that map above variable in @RequestMapping with the arguments passed which is "id"
+        return topicService.getTopic(id);
     }
 }
--- src/main/java/org/rohuu/springbootstarter/topic/TopicService.java
+++ src/main/java/org/rohuu/springbootstarter/topic/TopicService.java
@@ -8,12 +8,16 @@ import java.util.List;
 @Service
 public class TopicService {
      private final List<Topic> topics=Arrays.asList(
-             new Topic("Spring","Spring Framework","Spring framework description"),
-                new Topic("Java","Core Java","Core Java description"),
-                new Topic("JavaScript","JavaScript","JavaScript description")
+             new Topic("spring","Spring Framework","Spring framework description"),
+                new Topic("java","Core Java","Core Java description"),
+                new Topic("javaScript","JavaScript","JavaScript description")
         );
 
      public List getAllTopics(){
          return topics;
      }
+
+    public Topic getTopic(String id) {
+         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
 }
