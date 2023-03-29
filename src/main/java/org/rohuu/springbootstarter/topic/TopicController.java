8c5fb3d10cf9519a99db0e736e9b6fd096553dc6
Rohuu <rohuu99022@gmail.com>
2023-03-29 19:35:20 +0530

    creating a new resource using POST

--- src/main/java/org/rohuu/springbootstarter/topic/TopicController.java
+++ src/main/java/org/rohuu/springbootstarter/topic/TopicController.java
@@ -1,9 +1,7 @@
 package org.rohuu.springbootstarter.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,10 @@ public class TopicController {
     @RequestMapping("/topics/{id}") // parenthesis is for variable part
     public Topic getTopic(@PathVariable String id){ // @PathVariable tells spring that map above variable in @RequestMapping with the arguments passed which is "id"
         return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
     }
 }
--- src/main/java/org/rohuu/springbootstarter/topic/TopicService.java
+++ src/main/java/org/rohuu/springbootstarter/topic/TopicService.java
@@ -2,16 +2,17 @@ package org.rohuu.springbootstarter.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-     private final List<Topic> topics=Arrays.asList(
+     private final List<Topic> topics=new ArrayList<>(Arrays.asList(
              new Topic("spring","Spring Framework","Spring framework description"),
                 new Topic("java","Core Java","Core Java description"),
                 new Topic("javaScript","JavaScript","JavaScript description")
-        );
+        ));
 
      public List getAllTopics(){
          return topics;
@@ -19,5 +20,9 @@ public class TopicService {
 
     public Topic getTopic(String id) {
          return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
     }
 }
